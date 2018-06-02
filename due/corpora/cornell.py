1b7e393db35bb0ec0b0bff3d3fc2cdd5041a98e1
Dario <dario.chi@inventati.org>
2018-06-02 18:59:11 +0200

    Finalize Cornell corpus loader (fix #22)

--- due/corpora/cornell.py
+++ due/corpora/cornell.py
@@ -4,8 +4,10 @@ http://www.cs.cornell.edu/~cristian/Cornell_Movie-Dialogs_Corpus.html
 
 There are some givens to consider when using this corpus:
 
-* No **timestamp** information is provided in the dataset. Event timestamps
-should be considered fake.
+* No **timestamp** information is provided in the dataset. The first returned
+episode symbolically starts the day the paper was published, 15/6/2011, at noon,
+all the following events through the episodes are placed 1 second apart one
+another. 
 * The corpus only contains **one on one** conversations only.
 * Context-wise, episodes are **not self-contained**. That is, an episode in the
 corpus may start in the middle of a longer conversation, and may end before
@@ -14,10 +16,10 @@ that conversation is finished.
 
 import re
 import ast
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import pandas as pd
-import tqdm
+from tqdm import tqdm
 
 from due.episode import Episode
 from due.event import Event
@@ -25,16 +27,21 @@ from due import resource_manager
 
 rm = resource_manager
 
+START_DATE = datetime(2011, 6, 15, 12, 0)
+
+
 def load():
 	"""
 	Returns the Cornell Movie-Dialog Corpus as a list of
 	:class:`due.episode.Episode`s
 
-	TODO: better handle dates
+	TODO: return agents dictionary
 
 	:return: The Cornell Movie-Dialog Corpus
 	:rtype: `list` of :class:`due.episode.Episode`
 	"""
+	global current_date
+
 	result = []
 
 	with _open_cornell('movie_conversations.txt') as f:
@@ -48,13 +55,10 @@ def load():
 		df_lines = _read_cornell(f, ['line_id', 'character_id', 'movie_id', 'character_name', 'utterance'])
 		df_lines.set_index('line_id', inplace=True)
 
-	for conversation in df_conversations.itertuples():
+	current_date = START_DATE
+	for conversation in tqdm(df_conversations.itertuples(), total=len(df_conversations)):
 		events = [_build_event(l_id, df_lines) for l_id in conversation.utterance_list]
-		agents = set([conversation.character_id_A, conversation.character_id_B])
-		starter_agent = events[0].agent
-		invited_agent = (agents - set([starter_agent])).pop()
-		episode = Episode(starter_agent, invited_agent)
-		episode.events = events
+		episode = _build_episode(conversation, events)
 		result.append(episode)
 
 	return result
@@ -70,6 +74,18 @@ def _open_cornell(filename):
 def _read_cornell(file_buffer, columns):
 	return pd.read_csv(file_buffer, sep=re.escape(' +++$+++ '), names=columns)
 
+current_date = START_DATE
 def _build_event(line_id, df_lines):
+	global current_date
 	line = df_lines.loc[line_id]
-	return Event(Event.Type.Utterance, datetime.now(), line.character_id, line.utterance)
+	result = Event(Event.Type.Utterance, current_date, line.character_id, line.utterance)
+	current_date += timedelta(seconds=1)
+	return result
+
+def _build_episode(conversation, events):
+	agents = set([conversation.character_id_A, conversation.character_id_B])
+	starter_agent = events[0].agent
+	invited_agent = (agents - set([starter_agent])).pop()
+	result = Episode(starter_agent, invited_agent)
+	result.events = events
+	return result
